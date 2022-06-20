0dfd36246c25193d52bc90fb3a5b30863be2867d
AlenaBelenets <alenaharenkova@gmail.com>
2022-06-20 13:01:09 +0300

    Setup ViewController, Traffic Light is on

--- Traffic light/ViewController.swift
+++ Traffic light/ViewController.swift
@@ -13,16 +13,49 @@ class ViewController: UIViewController {
     @IBOutlet weak var yellowCircleLabel: UIView!
     @IBOutlet weak var greenCircleLabel: UIView!
 
-
     @IBOutlet weak var mainButton: UIButton!
+
+    private var currentColor = CurrentColor.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redCircleLabel.layer.cornerRadius = 75
+        yellowCircleLabel.layer.cornerRadius = 75
+        greenCircleLabel.layer.cornerRadius = 75
+        mainButton.layer.cornerRadius = 20
+
+        redCircleLabel.alpha = lightIsOff
+        greenCircleLabel.alpha = lightIsOff
+        yellowCircleLabel.alpha = lightIsOff
     }
 
 
     @IBAction func changeCircleButton(_ sender: Any) {
+        mainButton.setTitle("NEXT", for: .normal)
+
+        switch currentColor {
+        case .red:
+            redCircleLabel.alpha = lightIsOn
+            greenCircleLabel.alpha = lightIsOff
+            currentColor = .yellow
+        case .yellow:
+            redCircleLabel.alpha = lightIsOff
+            yellowCircleLabel.alpha = lightIsOn
+            currentColor = .green
+        case .green:
+            greenCircleLabel.alpha = lightIsOn
+            yellowCircleLabel.alpha = lightIsOff
+            currentColor = .red
+        }
     }
 }
 
+extension ViewController {
+    enum CurrentColor {
+        case red, yellow, green
+    }
+}
